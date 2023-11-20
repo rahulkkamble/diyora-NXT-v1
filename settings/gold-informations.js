e38f10bddf987f23bbe985a62a6a7a4b3f3cfbfa
Rahul <faizalimulla@gmail.com>
2023-11-20 18:39:54 +0530

    gold category page done

--- settings/gold-informations.js
+++ settings/gold-informations.js
@@ -1,31 +1,30 @@
-// ********************************************** admin-dashboard **********************************************
-// backup table
+// Gold Category table
 // Function to create a table row for each data item
 function createTableRow(data) {
     var row = document.createElement('tr');
     row.innerHTML = `
         <td class="align-middle">
-            <p class="text-xs ps-2 fw-bold mb-0">${data.backupNumber}</p>
-        </td>
-        <td>
-            <div class="d-flex ps-2 py-1">
-                <div class="d-flex flex-column justify-content-center">
-                    <h6 class="mb-0 text-sm">${data.backupName}</h6>
-                </div>
-            </div>
+            <p class="text-xs text-center ps-2 fw-bold mb-0">${data.goldNo}</p>
         </td>
         <td class="align-middle">
-            <p class="text-xs fw-bold mb-0"><a class="d-flex justify-content-center align-items-center" href="https://google.com"><img width="24px" height="24px" src="https://img.icons8.com/ios/50/download--v1.png" alt="download--v1"/></a></p>
+        <p class="text-xs ps-2 fw-bold mb-0">${data.goldCategory}</p>
+        </td>
+        <td class="align-middle">
+            <p class="text-xs text-wrap fw-bold mb-0">${data.goldCategoryDescription}</p>
         </td>
         <td class="align-middle text-center text-sm">
-            <span class="badge badge-sm"><img width="24" height="24" src="https://img.icons8.com/material-outlined/24/filled-trash.png" alt="filled-trash"/></span>
+            <span class="badge badge-sm"><div class="btn-group" role="group" aria-label="Basic mixed styles example">
+            <button type="button" class="btn btn-primary">View</button>
+            <button type="button" class="btn btn-success">Edit</button>
+            <button type="button" class="btn btn-danger">Delete</button>
+          </div></span>
         </td>
     `;
     return row;
 }
 
 // Populate the table with dynamic data by json file orders.json
-var backupTableBody = document.querySelector('#backup-table tbody');
+var backupTableBody = document.querySelector('#gold-category-table tbody');
 
 async function getData() {
     const response = await fetch('gold-informations.json');
@@ -43,7 +42,7 @@ async function processData2() {
         });
 
         // Initialize DataTables
-        var dataTable = new DataTable(document.querySelector('#backup-table'), {
+        var dataTable = new DataTable(document.querySelector('#gold-category-table'), {
             "pageLength": 5,
             "language": {
                 "lengthMenu": "Show <select class='form-select form-select-sm'>" +
